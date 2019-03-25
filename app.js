6f97ac6eb47e0b6a6a61465973cb80a77de60666
Oscar Polanco <oscar.polanco@outlook.com>
2019-03-25 01:47:47 -0500

    working on gcp deployment

--- app.js
+++ app.js
@@ -1,22 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const config = require('config');
+const cors = require('cors');
+
 // validate required environment variables
-[
-    'XPIMA_DB_URI'
-].forEach(item => {
+const envVars = ['XPIMA_DB_URI'];
+envVars.forEach(item => {
     if (!process.env[item]) {
         console.error(`Environment variable '${item}' is not defined!`);
         process.exit(1);
     }
 });
 
-const express = require('express');
-const mongoose = require('mongoose');
-const config = require('config');
-const cors = require('cors');
-
-const app = express();
-
 // mongoose connection
-const dbUri = process.env.XPIMA_DB_URI + config.get('db.name');
+const dbUri = process.env.XPIMA_DB_URI;
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 mongoose
@@ -24,21 +21,27 @@ mongoose
         useNewUrlParser: true
     })
     .then(() => {
-        console.log(`Successfully connected to ${dbUri}`);
+        console.log(`\nSuccessfully connected to ${dbUri}`);
         if (process.argv[2] === 'seed') {
             require('./startup/seed/execute-seed');
         }
     })
     .catch((err) => {
-        console.log(`Error connecting to ${dbUri}`);
+        console.log(`\nError connecting to ${dbUri}\n`);
         console.error(err);
         process.exit(1);
     });
 
+const app = express();
 //
 app.use(express.json());
-app.use(cors({origin: '*'}));
+app.use(
+    cors({
+        origin: '*'
+    })
+);
 require('./startup/routes')(app);
 
+app.envVars = envVars;
 
 module.exports = app;
--- server.js
+++ server.js
@@ -7,5 +7,9 @@ const port = process.env.PORT || 5000;
 const now = new Date();
 app.listen(port, () => {
     console.log(`Listening on port ${port} at ${now} ...`);
-    console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
+    console.log(`\nEnvironment Variables:`);
+    console.log(`  NODE_ENV: ${process.env.NODE_ENV}`);
+    app.envVars.forEach(item => {
+        console.error(`  ${item}: ${process.env[item]}`);
+    });
 });
